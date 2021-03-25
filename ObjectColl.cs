630cc81ce6d49fcbbe5d47a66aa57b9eafd72992
EricoSilitonga <81163445+EricoSilitonga@users.noreply.github.com>
2021-03-25 20:25:25 +0700

    Add files via upload

---
+++ ObjectColl.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectColl : MonoBehaviour
+{
+    private PlayerMovement _player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        _player = GameObject.Find("Player").GetComponent<PlayerMovement>();
+        if (collision.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
